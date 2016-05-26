eb74425fbb07b1f05fd47d5b9ba77cc940a061ba
tsvika <btsvika@gmail.com>
2016-05-26 21:19:36 +0300

    typo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
-var coffeeSources = ['components/coffee/tagline.coffee']
+var coffeeSources = ['components/coffee/tagline.coffee'];
 //var coffeeSources = ['components/coffee/*.coffee']
 
 gulp.task('coffee', function () {
