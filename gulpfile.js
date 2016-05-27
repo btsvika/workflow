a9b6f677a6b99a49e0f3629b3c31abed6b960a6a
tsvika <btsvika@gmail.com>
2016-05-27 12:42:45 +0300

    adding comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
+    /*
+         --save-dev
+         put in package.json the Dependencies under devDependencies
+    */
+
+    //npm install --save-dev gulp
 var gulp = require('gulp'),
+    //npm install --save-dev gulp-util
     gutil = require('gulp-util'),
+    //npm install --save-dev gulp-coffee
     coffee = require('gulp-coffee'),
+    //npm install --save-dev mustache
+    //npm install --save-dev jquery
+    //npm install --save-dev gulp-browserify
     browserify = require('gulp-browserify'),
+    //npm install --save-dev gulp-concat
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-//var coffeeSources = ['components/coffee/*.coffee']
+//var coffeeSources = ['components/coffee/*.coffee'] - for all the files
 var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
@@ -25,5 +37,6 @@ gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
+        //browserify() added the first line in scripts.js and jquery + mustache
         .pipe(gulp.dest('builds/development/js'))
 });
