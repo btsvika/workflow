b6c84384e57009a7632c6eeb4e0c5e375948d55a
tsvika <btsvika@gmail.com>
2016-05-27 21:08:28 +0300

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-    /*
-         --save-dev
-         put in package.json the Dependencies under devDependencies
-    */
+/*
+ --save-dev
+ put in package.json the Dependencies under devDependencies
+ */
 
-    //npm install --save-dev gulp
+//npm install --save-dev gulp
 var gulp = require('gulp'),
-    //npm install --save-dev gulp-util
+//npm install --save-dev gulp-util
     gutil = require('gulp-util'),
-    //npm install --save-dev gulp-coffee
+//npm install --save-dev gulp-coffee
     coffee = require('gulp-coffee'),
-    //npm install --save-dev mustache
-    //npm install --save-dev jquery
-    //npm install --save-dev gulp-browserify
+//npm install --save-dev mustache
+//npm install --save-dev jquery
+//npm install --save-dev gulp-browserify
     browserify = require('gulp-browserify'),
-    //npm install --save-dev gulp-concat
+//npm install --save-dev gulp-compass
+    compass = require('gulp-compass'),
+//npm install --save-dev gulp-concat
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -24,6 +26,7 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 
 gulp.task('coffee', function () {
@@ -33,10 +36,22 @@ gulp.task('coffee', function () {
         .pipe(gulp.dest('components/scripts'))
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
         //browserify() added the first line in scripts.js and jquery + mustache
         .pipe(gulp.dest('builds/development/js'))
+});
+
+//https://www.npmjs.com/package/gulp-compass
+gulp.task('compass', function () {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
 });
