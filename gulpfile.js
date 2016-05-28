c05fe57c56ddb599c63317f946c17b6f165f144a
tsvika <btsvika@gmail.com>
2016-05-28 13:54:59 +0300

    Added watches for static file

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,8 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 
 gulp.task('coffee', function () {
@@ -71,9 +73,11 @@ gulp.task('compass', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(coffeeSources, ['coffee']),
-    gulp.watch(jsSources, ['js']),
-    gulp.watch('components/sass/*.scss', ['compass'])
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function () {
@@ -83,5 +87,15 @@ gulp.task('connect', function () {
     })
 });
 
+gulp.task('html', function () {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function () {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
+
 //https://github.com/gulpjs/gulp/blob/master/docs/API.md
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
